d7f356501985949232268d28f88cfb28d170e68b
iqbal <iqbalnurimansyah@gmail.com>
2020-09-24 09:21:09 +0700

    errr... entah?

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var todoList : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        todoList = findViewById(R.id.todo_view)
+        todoList.layoutManager = LinearLayoutManager(this)
+        todoList.adapter = RowItemViewAdapter()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.todo_list_option, menu)
+        return true
     }
 }
